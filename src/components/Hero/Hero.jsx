803c9803fdcbd2318c8d79ddc2d616dfdb74fdce
Parker Stephenson <parkerstephenson00@gmail.com>
2023-12-08 18:19:15 -0800

    About Section
    Have most of the about section finished I just need to figure out how to resize my photo to get rid of the background and or have it fit correctly on the screen

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
+import styles from "./Hero.module.css";
+import { getImageUrl } from '../../utils';
 
 export const Hero = () => {
-  return (
-    <div>Hero</div>
-  )
-}
+  return <section className={styles.container}>
+    <div className={styles.content}><h1 className={styles.title}>Hi, I'm Parker</h1>
+    <p className={styles.description}>I'm a recent computer science graduate looking for my next oppurtunity
+        in the tech industry. Reach out if you'd like to learn more! 
+    </p>
+    <a href="mailto:parkerstephenson00@gmail.com" className={styles.contactBtn}>Contact Me</a>
+    </div>
+    <img src={getImageUrl("hero/heroImage.png")} alt="Hero image of me" className={styles.heroImg} />
+        <div className={styles.topBlur}></div>
+        <div className={styles.bottomBlur}></div>
+  </section>;
+};
